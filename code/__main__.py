67a7c07ed49facd204774477a91ebd818183127c
Alexander M Jones <amjones@live.co.uk>
2022-12-23 22:54:40 +0000

    Adds small error handling with token

--- code/__main__.py
+++ code/__main__.py
@@ -13,6 +13,10 @@ from queue_bot import QueueBot
 # Load in Discord token.
 load_dotenv()
 TOKEN = getenv('DISCORD_TOKEN')
+try:
+    assert TOKEN is not None
+except AssertionError:
+    raise EnvironmentError('No token found for the Discord bot in the .env file. Please see the readme for details.')
 
 # Create bot and run.
 bot = QueueBot(command_prefix='!')
